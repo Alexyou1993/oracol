21987b4099be67e2db8a66ddae0863c6dbeac0a5
iuraalexandru <alexiura93@yahoo.com>
2021-07-05 13:27:15 +0300

    add dependencies and redux files setup

---
+++ android/app/src/main/kotlin/com/example/oracol/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.oracol
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/reducer/reducer.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:oracol/src/models/index.dart';
+import 'package:redux/redux.dart';
+
+Reducer<AppState> reducer = combineReducers(<Reducer<AppState>>[
+  _reducer,
+]);
+
+AppState _reducer(AppState state, dynamic action){
+  return state.rebuild((AppStateBuilder b){
+
+  });
+}
