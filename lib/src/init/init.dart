3c15872685c59c4f84907839fed8f0ed9ad69735
iuraalexandru <alexiura93@yahoo.com>
2021-07-05 18:34:22 +0300

    add splash screen

---
+++ lib/src/init/init.dart
@@ -0,0 +1,16 @@
+import 'package:oracol/src/epics/app_epics.dart';
+import 'package:oracol/src/models/index.dart';
+import 'package:oracol/src/reducer/reducer.dart';
+import 'package:redux/redux.dart';
+import 'package:redux_epics/redux_epics.dart';
+
+Future<Store<AppState>> init() async {
+  const AppEpics epic = AppEpics();
+  return Store<AppState>(
+    reducer,
+    initialState: AppState.initialState(),
+    middleware: <Middleware<AppState>>[
+      EpicMiddleware<AppState>(epic.epics),
+    ],
+  );
+}
